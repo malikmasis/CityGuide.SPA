d7ad3ffaedea423212d60ae55fdd1d3f78bc35b1
malik masis <m.masis@amatis.nl>
2019-05-22 17:28:59 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
+import { CityDetailComponent } from './city/city-detail/city-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      CityComponent,
+      CityDetailComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
